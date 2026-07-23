81fc27c66cfa1dca72e8e80c2ada5d5af293fda9
Wilmar Galvis <wagalvis24@gmail.com>
2026-07-22 20:34:05 -0500

    continuamos...

--- 00- nivelacion/js/promesas.js
+++ 00- nivelacion/js/promesas.js
@@ -1,2 +1,23 @@
-const mensaje = "holaaa esto es una prueba";
+const sumar = (a,b) => {
+    return new Promise(
+        (resolve, reject) => {
+            if (a<0 || b<0){
+                reject("Esto no es válido...");
+            }
+            else{
+                resolve(a+b);
+            }
+        }
+    );
+
+};
+
+const resultado = sumar(-4, 3).
+    then((res) => {document.write(res)}).
+        catch((error) =>{document.write(error)});
+
+
+
+//console.log(resultado)
+//document.write(resultado)
 
